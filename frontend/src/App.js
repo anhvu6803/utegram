ea4de7e0e6613e9ede89ed4b79c9d96468baba1c
wasabiQuoc <wasabi.together@gmail.com>
2024-10-07 17:19:51 +0700

    AV1-navBar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage/LoginPage';
+import OptionBar from './components/OptionBar';
 const App = () => {
-  return <Router>
-    <Route path="/">
-    <LoginPage />
-    </Route>
-  </Router>
+  return (
+    <Router>
+      <OptionBar />
+      <Routes>
+        <Route path="/" element={<LoginPage />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 export default App;
