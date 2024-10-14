bb147e48c23cbc6dff9bcb5457945abc23c5ca5e
wasabiQuoc <wasabi.together@gmail.com>
2024-10-14 13:49:49 +0700

    FE_AV_4 - fix homepage

--- frontend/src/components/PostForm/PostForm.js
+++ frontend/src/components/PostForm/PostForm.js
@@ -23,9 +23,7 @@ import BookmarkBorderOutlinedIcon from '@mui/icons-material/BookmarkBorderOutlin
 import BookmarkOutlinedIcon from '@mui/icons-material/BookmarkOutlined';
 
 const PostForm = ({ postId }) => {
-    const item = itemData.find((item) => item.id ===  postId);
-
-    console.log(postId);
+    const item = itemData.find((item) => item.id === postId);
 
     const [liked, setLiked] = useState(false); // Boolean state for a single item
 
